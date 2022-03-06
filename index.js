67a585aab0f9b2b13d8efe45dc3b462109a82984
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-03-06 21:20:06 +0200

    add models

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const cors = require("cors");
+const models = require("./models/models");
 const router = require("./routes/index");
 
 const PORT = process.env.PORT || 5001;
---
+++ models/models.js
@@ -0,0 +1,41 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  firstName: { type: DataTypes.STRING, allowNull: false },
+  lastName: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.ENUM(["teacher", "student"]), allowNull: false },
+});
+
+const Group = sequelize.define("group", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
+  identificator: { type: DataTypes.STRING, allowNull: false }
+});
+
+User.hasMany(Group);
+Group.belongsTo(User);
+
+const Post = sequelize.define("post", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  caption: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING },
+});
+
+Group.hasMany(Post);
+Post.belongsTo(Group);
+
+const Form = sequelize.define("form", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  content: { type: DataTypes.STRING, allowNull: false }
+});
+
+User.hasMany(Form);
+Form.belongsTo(User);
+
+Post.hasMany(Form);
+Form.belongsTo(Post);
