4480de8428913206607b9df6fbf05560fea955f7
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-03-06 17:54:22 +0200

    add db connection

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server is up on port: ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server is up on port: ${PORT}`));
+  } catch(err) {
+    console.error(err.message);
+  }
+};
+
+start();
