98818a7b0ec124dfb574d3c4141ed12ee72e7530
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2022-03-08 11:12:20 +0200

    static init

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 const models = require("./models/models");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
 app.use(errorHandler);
 
